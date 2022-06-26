dfa5ac8e08d774ed3c03a2e5487fce6ec5dcbbed
Atakan Demircioğlu <31544037+atakde@users.noreply.github.com>
2022-06-26 16:11:30 +0300

    minor updates

--- controllers/markdownController.js
+++ controllers/markdownController.js
@@ -1,24 +1,29 @@
-const TurndownService = require('turndown');
-const { JSDOM } = require('jsdom');
+const TurndownService = require("turndown");
+const { JSDOM } = require("jsdom");
 const turndownService = new TurndownService();
 // import axios
-const axios = require('axios');
+const axios = require("axios");
 
-turndownService.addRule('code blocks', {
-  filter: 'pre',
+turndownService.addRule("code blocks", {
+  filter: "pre",
   replacement: function (content, node) {
-    return "```\n" + content + "\n```"
-  }
-})
+    return "```\n" + content + "\n```";
+  },
+});
 
 const getMarkdownFromUrl = async (url) => {
-  const response = await axios.get(url);
-  const dom = new JSDOM(response.data);
-  const article = dom.window.document.querySelector('article');
-  // remove article header
-  article.querySelector('.pw-post-byline-header').remove();
-  const markdown = turndownService.turndown(article);
-  return markdown;
+  try {
+    const response = await axios.get(url);
+    const dom = new JSDOM(response.data);
+    const article = dom.window.document.querySelector("article");
+    // remove article header
+    article.querySelector(".pw-post-byline-header").remove();
+    const markdown = turndownService.turndown(article);
+    return markdown;
+  } catch (error) {
+    console.log(error);
+    return false;
+  }
 };
 
 exports.getMarkdownFromUrl = getMarkdownFromUrl;
--- public/js/main.js
+++ public/js/main.js
@@ -1,25 +1,31 @@
-const form = document.querySelector('form');
-const urlInput = document.querySelector('#urlInput');
+const form = document.querySelector("form");
+const urlInput = document.querySelector("#urlInput");
 
-form.addEventListener('submit', (e) => {
-  document.querySelector('.loader').classList.remove('d-none');
+form.addEventListener("submit", (e) => {
+  document.querySelector(".loader").classList.remove("d-none");
   e.preventDefault();
   const url = urlInput.value;
   // fetch post request
-  fetch('/markdown', {
-    method: 'POST',
+  fetch("/markdown", {
+    method: "POST",
     headers: {
-      'Content-Type': 'application/json'
+      "Content-Type": "application/json",
     },
     body: JSON.stringify({
-      url: url
-    })
+      url: url,
+    }),
   })
-    .then(res => res.json())
-    .then(data => {
-      document.querySelector('.loader').classList.add('d-none');
-      document.querySelector('.form-floating').classList.remove('d-none');
-      document.querySelector('.form-floating textarea').innerHTML = data.markdown;
+    .then((res) => res.json())
+    .then((data) => {
+      document.querySelector(".loader").classList.add("d-none");
+      if (data.markdown) {
+        document.querySelector(".form-floating").classList.remove("d-none");
+        document.querySelector(".form-floating textarea").innerHTML =
+          data.markdown;
+      } else {
+        document.querySelector(".error").classList.remove("d-none");
+        document.querySelector(".error").innerHTML = "Error ocurred!";
+      }
     })
-    .catch(err => console.log(err));
+    .catch((err) => console.log(err));
 });
