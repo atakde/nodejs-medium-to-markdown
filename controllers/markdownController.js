7ca4d0e2c6e5c86febe6eb3ca2d258b52bfe4c3d
Atakan Demircioğlu <31544037+atakde@users.noreply.github.com>
2022-06-26 15:59:59 +0300

    added axios

--- controllers/markdownController.js
+++ controllers/markdownController.js
@@ -1,6 +1,8 @@
 const TurndownService = require('turndown');
 const { JSDOM } = require('jsdom');
 const turndownService = new TurndownService();
+// import axios
+const axios = require('axios');
 
 turndownService.addRule('code blocks', {
   filter: 'pre',
@@ -10,9 +12,8 @@ turndownService.addRule('code blocks', {
 })
 
 const getMarkdownFromUrl = async (url) => {
-  const response = await fetch(url);
-  const html = await response.text();
-  const dom = new JSDOM(html);
+  const response = await axios.get(url);
+  const dom = new JSDOM(response.data);
   const article = dom.window.document.querySelector('article');
   // remove article header
   article.querySelector('.pw-post-byline-header').remove();
